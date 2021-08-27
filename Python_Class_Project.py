28df94793090a1889c582625e273aac2c12969cb
eugene <eugene_ong_jun_wei@connect.ite.edu.sg>
2021-08-27 18:11:36 +0800

    GET request, Return Status, Website header, Mobile

--- Python_Class_Project.py
+++ Python_Class_Project.py
@@ -1,5 +1,26 @@
+#Perform Get request on website
 import requests
+url = 'https://brickset.com/sets/year-2005'
+r = requests.get(url)
+print(r.text)
 
-#testing 1
+#Displays OK retun status
+print("Status code:")
+print("\t*",r.status_code)
 
-#testing 2
+#Display website header
+h = requests.head(url)
+print("Header:")
+print("**********")
+for x in h.headers:
+    print("\t",x,":",h.headers[x])
+print("**********")
+
+#modify the Header user-agent to display "mobile"
+headers = {
+    'User-Agent':'Iphone X'
+}
+url2 = 'http://httpbin.org/headers'
+rh = requests.get(url2, headers=headers)
+print(rh.text)
+
