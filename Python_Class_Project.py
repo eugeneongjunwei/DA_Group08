2c783c2713c2c1c5268ffcfb6c01b91d419e5bf5
javier <javier_tan_jing_yang@connect.ite.edu.sg>
2021-08-29 18:55:03 +0800

    Second commit

--- Python_Class_Project.py
+++ Python_Class_Project.py
@@ -24,6 +24,31 @@ url2 = 'http://httpbin.org/headers'
 rh = requests.get(url2, headers=headers)
 print(rh.text)
 
+import scrapy
+
+class NewSpider(scrapy.Spider):
+    name = "new_spider"
+
+    start_urls = ['http://10.10.11.178/index.html']
+    def parse(self, response):
+        css_selector = 'img'
+        for x in response.css(css_selector):
+            newsel = '@src'
+
+            yield{
+                'Image Link': x.xpath(newsel).extract_first(),
+            }
+
+        Page_selector = '.next a ::attr(href)'
+
+        next_page = response.css(Page_selector).extract_first()
+
+        if next_page:
+            yield scrapy.Request(
+                response.urljoin(next_page),
+                callback=self.parse
+            )
+
 import unittest
 
 class Python_Class_Project(unittest.TestCase):
